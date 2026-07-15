514081d054f6c30a3cc815d54a10cfff4158f253
xzx <3505488164@qq.com>
2026-07-15 21:50:30 +0800

    remove mc source

--- src/main/java/net/neoforged/api/distmarker/Dist.java
+++
@@ -1,5 +0,0 @@
-package net.neoforged.api.distmarker;
-
-public enum Dist {
-    CLIENT,SERVER
-}
--- src/main/java/net/neoforged/neoforgespi/language/IModInfo.java
+++
@@ -1,6 +0,0 @@
-package net.neoforged.neoforgespi.language;
-
-public interface IModInfo {
-
-    String getDisplayName();
-}
